479c7620723ef1862014e1a0ac8e5462b5e8e48c
HussnainQuresshi <hussnain77133@gmail.com>
2022-01-05 20:59:52 +0500

    added the proxy middleware to add the missing headers on req

--- client/src/context/UserProvider.js
+++ client/src/context/UserProvider.js
@@ -1,132 +1,141 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-export const UserContext = React.createContext()
+export const UserContext = React.createContext();
 
-const userAxios = axios.create()
+const userAxios = axios.create();
 
-userAxios.interceptors.request.use(config => {
-  const token = localStorage.getItem("token")
-  config.headers.Authorization = `Bearer ${token}`
-  return config
-})
+userAxios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
 
-export default function UserProvider(props){
-  const initState = { 
-    user: JSON.parse(localStorage.getItem("user")) || {}, 
-    token: localStorage.getItem("token") || "", 
+export default function UserProvider(props) {
+  const initState = {
+    user: JSON.parse(localStorage.getItem("user")) || {},
+    token: localStorage.getItem("token") || "",
     listings: [],
-    errMsg: ""
-  }
+    errMsg: "",
+  };
 
-  const [userState, setUserState] = useState(initState)
+  const [userState, setUserState] = useState(initState);
 
-  function signup(credentials){
-    axios.post("/auth/signup", credentials)
-      .then(res => {
-        const { user, token } = res.data
-        localStorage.setItem("token", token)
-        localStorage.setItem("user", JSON.stringify(user))
-        setUserState(prevUserState => ({
+  function signup(credentials) {
+    axios
+      .post("/auth/signup", credentials)
+      .then((res) => {
+        const { user, token } = res.data;
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+        setUserState((prevUserState) => ({
           ...prevUserState,
           user,
-          token
-        }))
+          token,
+        }));
       })
-      .catch(err => handleAuthErr(err.response.data.errMsg))
+      .catch((err) => handleAuthErr(err.response.data.errMsg));
   }
 
-  function login(credentials){
-    axios.post("/auth/login", credentials)
-      .then(res => {
-        const { user, token } = res.data
-        localStorage.setItem("token", token)
-        localStorage.setItem("user", JSON.stringify(user))
-        getUserListings()
-        setUserState(prevUserState => ({
+  function login(credentials) {
+    axios
+      .post("/auth/login", credentials)
+      .then((res) => {
+        const { user, token } = res.data;
+        localStorage.setItem("token", token);
+        localStorage.setItem("user", JSON.stringify(user));
+        getUserListings();
+        setUserState((prevUserState) => ({
           ...prevUserState,
           user,
-          token
-        }))
+          token,
+        }));
       })
-      .catch(err => handleAuthErr(err.response.data.errMsg)
-      )
-  //    return getUserListings()
+      .catch((err) => handleAuthErr(err.response.data.errMsg));
+    //    return getUserListings()
   }
 
-  function logout(){
-    localStorage.removeItem("token")
-    localStorage.removeItem("user")
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
     setUserState({
       user: {},
       token: "",
-      listings: []
-    })
+      listings: [],
+    });
   }
 
-  function handleAuthErr(errMsg){
-    setUserState(prevState => ({
+  function handleAuthErr(errMsg) {
+    setUserState((prevState) => ({
       ...prevState,
-      errMsg
-    }))
+      errMsg,
+    }));
   }
 
-  function resetAuthErr(){
-    setUserState(prevState => ({
+  function resetAuthErr() {
+    setUserState((prevState) => ({
       ...prevState,
-      errMsg: ""
-    }))
+      errMsg: "",
+    }));
   }
 
-  function getUserListings(){
-    userAxios.get("/api/listing/user")
-      .then(res => {
-        setUserState(prevState => ({
+  function getUserListings() {
+    userAxios
+      .get("/api/listing/user")
+      .then((res) => {
+        setUserState((prevState) => ({
           ...prevState,
-          listings: res.data
-        }))
+          listings: res.data,
+        }));
       })
-      .catch(err => console.log(err.response.data.errMsg))
+      .catch((err) => console.log(err.response.data.errMsg));
   }
 
-  function addListing(newListing){
-    userAxios.post("/api/listing", newListing)
-      .then(res => {
-        setUserState(prevState => ({
+  function addListing(newListing) {
+    userAxios
+      .post("/api/listing", newListing)
+      .then((res) => {
+        setUserState((prevState) => ({
           ...prevState,
-          listings: [...prevState.listings, res.data]
-        }))
+          listings: [...prevState.listings, res.data],
+        }));
       })
-      .catch(err => console.log(err.response.data.errMsg)
-      )
-      return getUserListings()
+      .catch((err) => console.log(err.response.data.errMsg));
+    return getUserListings();
   }
 
   // delete listing
   function deleteListing(listingId) {
-    userAxios.delete(`/api/listing/${listingId}`)
-      .then(res => setUserState(prevState => ({
-        ...prevState,
-        listings: prevState.listings.filter(listing => listing._id !== listingId)
-      })))
-      .catch(err => console.log(err)
+    userAxios
+      .delete(`/api/listing/${listingId}`)
+      .then((res) =>
+        setUserState((prevState) => ({
+          ...prevState,
+          listings: prevState.listings.filter(
+            (listing) => listing._id !== listingId
+          ),
+        }))
       )
-      return getUserListings()
+      .catch((err) => console.log(err));
+    return getUserListings();
   }
 
-  // edit listing 
-  function editListing(newEntry, listingId){
-    userAxios.put(`/api/listing/${listingId}`, newEntry)
-    .then(res => setUserState(prevState => ({
-      ...prevState,
-      listings: prevState.listings.map(listing => listing._id !== listingId? listing: res.data)
-    }))
-    )
-    return getUserListings()
+  // edit listing
+  function editListing(newEntry, listingId) {
+    userAxios.put(`/api/listing/${listingId}`, newEntry).then((res) =>
+      setUserState((prevState) => ({
+        ...prevState,
+        listings: prevState.listings.map((listing) =>
+          listing._id !== listingId ? listing : res.data
+        ),
+      }))
+    );
+    return getUserListings();
   }
 
-  
-    
+  useEffect(() => {
+    getUserListings();
+  }, []);
 
   return (
     <UserContext.Provider
@@ -138,9 +147,10 @@ export default function UserProvider(props){
         addListing,
         deleteListing,
         editListing,
-        resetAuthErr
-      }}>
-      { props.children }
+        resetAuthErr,
+      }}
+    >
+      {props.children}
     </UserContext.Provider>
-  )
+  );
 }
--- server.js
+++ server.js
@@ -1,39 +1,63 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const morgan = require('morgan')
-const mongoose = require('mongoose')
-const expressJwt = require('express-jwt')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const expressJwt = require("express-jwt");
 
 // to get SECRET Variable from .env file
-process.env.SECRET
+process.env.SECRET;
 
-app.use(express.json())
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(morgan("dev"));
 
 mongoose.connect(
-  'mongodb://localhost:27017/ebaylistinggendb',
+  "mongodb://localhost:27017/ebaylistinggendb",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
   },
-  () => console.log('Connected to the DB')
-)
+  () => console.log("Connected to the DB")
+);
 
-app.use('/auth', require('./routes/authRouter.js'))
-app.use('/api', expressJwt({secret: process.env.SECRET, algorithms: ['HS256']})) // req.user
-app.use('/api/listing', require('./routes/listingRouter.js'))
+// Add headers so that no proxy error arrives
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect can be * if you are not sure about the port
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // Request methods you wish to allow can be * if you are not sure about the request method
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow can be * if you are not sure about the request headers
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+});
+
+app.use("/auth", require("./routes/authRouter.js"));
+app.use(
+  "/api",
+  expressJwt({ secret: process.env.SECRET, algorithms: ["HS256"] })
+); // req.user
+app.use("/api/listing", require("./routes/listingRouter.js"));
 
 app.use((err, req, res, next) => {
-  console.log(err)
-  if(err.name === "UnauthorizedError"){
-    res.status(err.status)
+  console.log(err);
+  if (err.name === "UnauthorizedError") {
+    res.status(err.status);
   }
-  return res.send({errMsg: err.message})
-})
+  return res.send({ errMsg: err.message });
+});
 
 app.listen(9000, () => {
-  console.log(`Server is running on local port 9000`)
-})
+  console.log(`Server is running on local port 9000`);
+});
